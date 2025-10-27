378184eafba3397a4e97c1e1424d2d51d2ccde44
Furkan Vural <furkan.vural@hotmail.de>
2025-10-27 17:07:39 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./index.css";
+
+export default function App() {
+    return (
+        <div className="bg-blue-600 text-white min-h-screen flex items-center justify-center text-6xl font-bold">
+            TAILWIND LÄUFT 🔥
+        </div>
+    );
+}
